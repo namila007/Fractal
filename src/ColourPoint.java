c95c9d24115d39966fca93cecfc6188e7e4b44a8
Namila Bandara <namila007@gmail.com>
2017-08-05 11:15:04 +0530

    Version 2, working fractals

---
+++ src/ColourPoint.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+/**
+ * Created by Namila on 8/4/2017.
+ */
+public class ColourPoint {
+    private int iteration;
+    private double zabs;
+
+
+
+    public ColourPoint(int iteration,double zabs){
+        this.iteration=iteration;
+        this.zabs=zabs;
+    }
+
+
+
+    public int getIteration() {
+        return iteration;
+    }
+
+    public double getZabs() {
+        return zabs;
+    }
+}
--- src/Mandelbrot.java
+++ src/Mandelbrot.java
@@ -3,20 +3,20 @@
  */
 
 
-public class Mandelbrot extends Fractal {
+public class Mandelbrot extends AbstractFractal {
     Complex c;
     Complex z;
 
-    public Mandelbrot(Complex number){
-        this.c =number;
-        this.z =new Complex(0,0);
+    public Mandelbrot( int frame_height,int frame_width,double real_max, double real_min,
+                      double imag_max, double imag_min, int max_iterate){
+        super(frame_height,frame_width,real_max,real_min, imag_max,imag_min,max_iterate,0,0);
     }
 
 
     @Override
+    ColourPoint setPoints(int zx, int zy, double cx, double cy) {
+        return getIterations(new Complex(0d,0d),getComplexPoint(zx,zy));
 
-    public Complex setComplexValues(int x, int y) {
-        return new Complex(x,y);
     }
 
 
